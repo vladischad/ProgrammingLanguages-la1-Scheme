0c751a69d15ad9a2aa0746281cb7fd13f2b5cc10
Vladyslav (Vlad) Maliutin <vladmaliutin@u.boisestate.edu>
2025-02-13 21:41:44 -0700

    1.0

---
+++ super-duper.scm
@@ -0,0 +1,28 @@
+#lang racket
+
+(define (duplicate-element elem count)
+  (if (= count 0)
+      '()
+      (cons elem (duplicate-element elem (- count 1)))))
+
+(define (super-duper source count)
+  (cond
+    ((not (list? source)) source)
+    ((null? source) '())
+    ((list? (car source)) 
+     (cons (super-duper (car source) count) 
+           (super-duper (cdr source) count)))
+    (else 
+     (append (duplicate-element (car source) count) 
+             (super-duper (cdr source) count)))))
+
+;; Test cases
+(display (super-duper 123 1)) (newline)
+(display (super-duper 123 2)) (newline)
+(display (super-duper '() 1)) (newline)
+(display (super-duper '() 2)) (newline)
+(display (super-duper '(x) 1)) (newline)
+(display (super-duper '(x) 2)) (newline)
+(display (super-duper '(x y) 1)) (newline)
+(display (super-duper '(x y) 2)) (newline)
+(display (super-duper '((a b) y) 3)) (newline)
